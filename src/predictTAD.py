d93e2578896849a382a4c2ddedfcf9b344b2c34e
shen <490788573@qq.com>
2024-09-22 21:50:52 +0800

    Update predictTAD.py

--- src/predictTAD.py
+++ src/predictTAD.py
@@ -1,10 +1,14 @@
 from sys import argv
-from utils import predictTAD,predictTADmultiRes
-
+from utils import predictTADmultiRes_,predictTADmultiRes 
+import time      
+                 
 script,model,latentTADFiles,resolutions,n,outPrefix = argv
 modelLists = model.split(',')
 latentTADLists = latentTADFiles.split(',')
 reslist = [int(x) for x in resolutions.split(',')]
 n = int(n)
 
-predictTADmultiRes(modelLists,latentTADLists,reslist,n,outPrefix)
+if n == 0:
+    predictTADmultiRes_(modelLists,latentTADLists,reslist,outPrefix)
+else:
+    predictTADmultiRes(modelLists,latentTADLists,reslist,n,outPrefix)
